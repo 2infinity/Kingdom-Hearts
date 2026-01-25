886d253046b3ed0677310213fb2f74ed7d8bb504
2infinity <tommytjab@gmail.com>
2026-01-25 17:38:35 -0600

    update

--- javaScript/index/index.js
+++ javaScript/index/index.js
@@ -1,9 +1,13 @@
 const menuBtn = document.getElementById("menuBtn");
 const bar1 = document.getElementById("bar1");
 const bar2 = document.getElementById("bar2");
+const menu = document.getElementById("menu")
+const menuIcon2 = document.getElementById("menu-icon2");
 
 menuBtn.addEventListener("click", ()=>{
   bar1.classList.toggle("open");
   bar2.classList.toggle("open");
+  menu.classList.toggle("open");
+  menuIcon2.classList.toggle("open");
 });
 
