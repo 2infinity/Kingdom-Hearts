d8b64ee3698a679e0f9ebc24fe97ac3a061689e8
2infinity <tommytjab@gmail.com>
2026-02-03 20:00:52 -0600

    update

--- javaScript/khx/khx.js
+++ javaScript/khx/khx.js
@@ -1,12 +1,24 @@
 const menuBtn = document.getElementById("menuBtn");
 const bar1 = document.getElementById("bar1");
 const bar2 = document.getElementById("bar2");
+const menuIcon2 = document.getElementById("menu-icon2");
 
+
+menuBtn.addEventListener("mouseenter", () =>{
+  cursorMove.play();
+})
 
 
 menuBtn.addEventListener("click", ()=>{
   bar1.classList.toggle('open');
   bar2.classList.toggle("open");
+  menuIcon2.classList.toggle("open");
   
   
-})
+});
+
+
+//audio
+const getItem = document.getElementById("getItem");
+const selectItems = document.getElementById("selectItems");
+const cursorMove = document.getElementById("cursorMove");
