1a94358d5585f6c4441e80d0211133ad9479139a
2infinity <163491130+2infinity@users.noreply.github.com>
2026-08-01 19:31:48 -0500

    created a menu for the union cross page.

--- javaScript/khx/khx.js
+++ javaScript/khx/khx.js
@@ -2,6 +2,7 @@ const menuBtn = document.getElementById("menuBtn");
 const bar1 = document.getElementById("bar1");
 const bar2 = document.getElementById("bar2");
 const menuIcon2 = document.getElementById("menu-icon2");
+const menu = document.getElementById("menu")
 
 
 //audio\\
@@ -19,8 +20,10 @@ menuBtn.addEventListener("mouseenter", () =>{
 menuBtn.addEventListener("click", ()=>{
   bar1.classList.toggle('open');
   bar2.classList.toggle("open");
+  menu.classList.toggle("open");
   menuIcon2.classList.toggle("open");
   selectItems.currentTime = 0;
   selectItems.play();
 });
 
+// ALL SHOULD BE EXPORTED TO ALL OF THE SITES   VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
