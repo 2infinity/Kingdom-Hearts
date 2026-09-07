767e4a36e1b98a6c2ddd9819d75a0aa72667479e
2infinity <163491130+2infinity@users.noreply.github.com>
2026-09-07 09:13:02 -0500

    Created Kh4 Content

---
+++ javaScript/kh4/kh4.js
@@ -0,0 +1,36 @@
+const menuBtn = document.getElementById("menuBtn");
+const bar1 = document.getElementById("bar1");
+const bar2 = document.getElementById("bar2");
+const menuIcon2 = document.getElementById("menu-icon2");
+const menu = document.getElementById("menu")
+
+
+//audio\\
+const getItem = document.getElementById("getItem");
+const selectItems = document.getElementById("selectItems");
+const cursorMove = document.getElementById("cursorMove");
+
+
+menuBtn.addEventListener("mouseenter", () =>{
+  cursorMove.currentTime = 0.15;
+  cursorMove.play();
+});
+
+
+menuBtn.addEventListener("click", ()=>{
+  bar1.classList.toggle('open');
+  bar2.classList.toggle("open");
+  menu.classList.toggle("open");
+  menuIcon2.classList.toggle("open");
+  selectItems.currentTime = 0;
+  selectItems.play();
+});
+
+document.addEventListener("click", (e) => {
+  if (!menu.contains(e.target) && !menuIcon2.contains(e.target)) {
+    menu.classList.remove("open");
+    menuIcon2.classList.remove("open");
+    bar1.classList.remove("open");
+    bar2.classList.remove("open");
+  }
+});
