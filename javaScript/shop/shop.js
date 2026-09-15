e6e70e671a9d142482e45e81074be089448a28e2
2infinity <163491130+2infinity@users.noreply.github.com>
2026-09-15 10:49:41 -0500

    Fixed home btn bug and adjusted icons for mobile.

--- javaScript/shop/shop.js
+++ javaScript/shop/shop.js
@@ -2,7 +2,8 @@ const menuBtn = document.getElementById("menuBtn");
 const bar1 = document.getElementById("bar1");
 const bar2 = document.getElementById("bar2");
 const menuIcon2 = document.getElementById("menu-icon2");
-const menu = document.getElementById("menu")
+const menu = document.getElementById("menu");
+const menuBtns = document.querySelectorAll(".menuBtns");
 
 
 //audio\\
